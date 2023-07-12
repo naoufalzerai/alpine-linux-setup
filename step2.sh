e66fbf05a1d0435b36aa73d962f5e95fa50334da
naoufal zerai <naoufal.zerai@gmail.com>
2023-07-12 17:27:13 -0400

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -2,7 +2,7 @@
 
 apk add wget curl socat nmap net-tools build-base setxkbmap sudo xrandr bash zsh dbus dbus-x11 sudo setup-xorg-base xfce4 xfce4-terminal lightdm dbus-x11
 #apk add open-vm-tools open-vm-tools-guestinfo open-vm-tools-deploypkg open-vm-tools-gtk
-apk add slim i3wm i3status libxcb-dev i3lock xf86-video-vmware dmenu
+apk add lightdm-gtk-greeter i3wm i3status libxcb-dev i3lock xf86-video-vmware dmenu
 apk add mesa-gl glib feh firefox-esr accountsservice openvpn
 apk add docker docker-compose
 
