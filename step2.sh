1751cfd202b69aeb0be23d337eb7975d805bf182
naoufal zerai <naoufal.zerai@gmail.com>
2023-07-18 19:50:17 -0400

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -11,7 +11,7 @@ apk add flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # add user
-adduser near
+#adduser near
 
 cp ./near/.config/ /home/near/
 chown -R near:near /home/near
